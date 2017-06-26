7cd60183c865ec77f6c5d096b85f4de74a1f93cd
dregos <dregojevic@gmail.com>
2017-06-26 14:58:40 +0200

    Prvi

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Vivify blog</title>
+
+</head>
+<body class="main-page">
+<link rel="stylesheet" type="text/css" href="css/main.css" media="screen">
+  <header>
+      <!-- This is page’s header -->
+      <h2>VivifyBlog</h2>
+      <nav>
+        <a href="index.html" title="Homepage">Home</a>
+        <a href="about.html" title="About">About</a>
+        <a href="contact.html" title="Contact">Contact</a>
+        <a href="admin/admin.html" title="Admin">Admin</a>
+      </nav>
+  </header>
+  <main>
+    <!-- This is page’s main content -->
+    <h1 class="page-title">
+      HEADER - MV by DR/Vivify
+    </h1>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+
+    <button type="button" name="btnGetStarted">Get started today</button>
+
+
+  </main>
+
+  <div class="article-container">
+    <div>
+      <h1></h1>
+      <content>
+        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500Lorem Ipsum has been the industry's standard dummy text ever since the 1500</p>
+        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500Lorem Ipsum has been the industry's standard dummy text ever since the 1500</p>
+      </content>
+    </div>
+    <div></div>
+    <div></div>
+  </div>
+
+  <footer>
+    FOOTER Address
+  </footer>
+</body>
+</html>
